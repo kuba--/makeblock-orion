dcbb40f57b17bb3d0539a5aaf5cca03536828ab4
Kuba Podgórski <kuba.podgorski@gmail.com>
2016-08-28 00:26:55 +0200

    Refactor StopAndGo

--- StopAndGo/StopAndGo.ino
+++ StopAndGo/StopAndGo.ino
@@ -3,34 +3,49 @@
 
 #include <MeOrion.h>
 
+#define DEFAULT_INTERVAL_MS 200
+
 #define MAX_SOUND_STRENGTH 550
 #define MIN_DISTANCE_CM 20.0
+
 #define MAX_SOUND_INTERVAL_MS 1000
-#define DEFAULT_INTERVAL_MS 200
 #define TURN90_DELAY_MS 500
+
 #define SPEED 160
 
-typedef void (* Event)(void);
+#ifdef DEBUG
+#define DBG(...) Serial.println(__VA_ARGS__)
+#else
+#define DBG(...)
+#endif
 
-void onNoise();
-void onWall();
-void onState();
+#define Stop(...)      \
+    do {               \
+        MotorL.stop(); \
+        MotorR.stop(); \
+    } while (0)
 
-enum {
-  STOP,
-  TURN,
-  GO
-} State;
+#define Go(...)             \
+    do {                    \
+        MotorL.run(-SPEED); \
+        MotorR.run(SPEED);  \
+    } while (0)
 
-enum {
-  LEFT,
-  RIGHT
-} Direction;
-
-
-const int16_t Noises[2] = { 256, 512 };
-const Event Events[] = { onNoise, onWall, onState };
-const uint8_t NEvents =  sizeof(Events) / sizeof(Event);
+#define Turn90(...)                    \
+    do {                               \
+        int16_t l = -SPEED, r = SPEED; \
+        switch (Direction) {           \
+            case LEFT:                 \
+                l = -l;                \
+                break;                 \
+            case RIGHT:                \
+                r = -r;                \
+                break;                 \
+        }                              \
+        MotorL.run(l);                 \
+        MotorR.run(r);                 \
+        delay(TURN90_DELAY_MS);        \
+    } while (0)
 
 MeSoundSensor SoundSensor(PORT_8);
 MeRGBLed Led(PORT_3, SLOT_1, 15);
@@ -38,146 +53,114 @@ MeUltrasonicSensor UltrasonicSensor(PORT_4);
 MeDCMotor MotorL(M1);
 MeDCMotor MotorR(M2);
 
+enum { STOP, GO, TURN90 } State = STOP;
+enum { LEFT, RIGHT } Direction = LEFT;
+
+typedef void (*Task)(void);
+
+static void showLed(int16_t val) {
+    const int16_t noises[2] = {256, 512};
+    double f = 256.0;
+
+    if (val < noises[0]) {
+        f /= noises[0];
+
+        Led.setColor(0, 0, val * ceil(f));
+        Led.show();
+
+        return;
+    }
+
+    if (val < noises[1]) {
+        val -= noises[0];
+        f /= (noises[1] - noises[0]);
+
+        Led.setColor(0, val * ceil(f), 0);
+        Led.show();
+
+        return;
+    }
+
+    val -= noises[1];
+    if (val < 1) val = 1;
+    if (val > 255) val = 255;
+
+    Led.setColor(val, 0, 0);
+    Led.show();
+}
+
+void taskCheckNoise() {
+    static unsigned long lastcall = 0;
+    static unsigned long lastnoise = 0;
+    unsigned long now = millis();
+
+    if (now < lastcall + DEFAULT_INTERVAL_MS) return;
+
+    int16_t strength = SoundSensor.strength();
+    DBG("SoundSensor.strength:", strength);
+
+    showLed(strength);
+
+    if (strength > MAX_SOUND_STRENGTH &&
+        now >= lastnoise + MAX_SOUND_INTERVAL_MS) {
+        State = (State == STOP) ? GO : STOP;
+        lastnoise = now;
+    }
+
+    lastcall = now;
+}
+
+void taskAvoidWall() {
+    static unsigned long lastcall = 0;
+    unsigned long now = millis();
+
+    if (State == STOP || now < lastcall + DEFAULT_INTERVAL_MS) return;
+
+    double d = UltrasonicSensor.distanceCm();
+    DBG("UltrasonicSensor.distanceCm:", d);
+
+    if (d > 0.0 && d < MIN_DISTANCE_CM) {
+        if (State != TURN90) Direction = (0 == random(0, 2)) ? LEFT : RIGHT;
+        State = TURN90;
+    } else {
+        State = GO;
+    }
+
+    lastcall = now;
+}
+
+void taskChangeState() {
+    switch (State) {
+        case STOP:
+            Stop();
+            break;
+
+        case TURN90:
+            Turn90();
+            break;
+
+        case GO:
+            Go();
+            break;
+    }
+}
+
+const Task Tasks[] = {taskCheckNoise, taskAvoidWall, taskChangeState};
+const uint8_t NTasks = sizeof(Tasks) / sizeof(Task);
+
 void setup() {
 #ifdef DEBUG
-  Serial.begin(9600);
+    Serial.begin(9600);
 #endif
 
-  randomSeed(analogRead(0));
-
-  State = STOP;
-  Direction = LEFT;
+    randomSeed(analogRead(0));
 }
 
 void loop() {
-  static uint8_t i = 0;
+    static uint8_t i = 0;
 
-  Events[i]();
-  i = (i + 1) % NEvents;
-}
-
-static int16_t soundStrength() {
-  int16_t strength = SoundSensor.strength();
-#ifdef DEBUG
-  Serial.print("sound.strength: ");
-  Serial.println(strength);
-#endif
-  return strength;
-}
-
-static void ledShow(int16_t val) {
-  double f = 256.0;
-
-  if (val < Noises[0]) {
-    f /= Noises[0];
-
-    Led.setColor(0, 0, val * ceil(f));
-    Led.show();
-
-    return;
-  }
-
-  if (val < Noises[1]) {
-    val -= Noises[0];
-    f /= (Noises[1] - Noises[0]);
-
-    Led.setColor(0, val * ceil(f), 0);
-    Led.show();
-
-    return;
-  }
-
-  val -= Noises[1];
-  if (val < 1) val = 1;
-  if (val > 255) val = 255;
-
-  Led.setColor(val, 0, 0);
-  Led.show();
-}
-
-static double distanceCm() {
-  double d = UltrasonicSensor.distanceCm();
-#ifdef DEBUG
-  Serial.print("distance cm: ");
-  Serial.println(d);
-#endif
-  return d;
-}
-
-static void moveForward() {
-  MotorL.run(-SPEED);
-  MotorR.run(SPEED);
-}
-
-static void stopMotors() {
-  MotorL.stop();
-  MotorR.stop();
-}
-
-static void turn90() {
-  int16_t l = -SPEED, r = SPEED;
-  if (Direction == LEFT)
-    l = -l;
-  else
-    r = -r;
-
-  MotorL.run(l);
-  MotorR.run(r);
-  delay(TURN90_DELAY_MS);
-}
-
-void onNoise() {
-  static unsigned long lastcall = 0;
-  static unsigned long lastnoise = 0;
-  unsigned long now = millis();
-
-  if (now < lastcall + DEFAULT_INTERVAL_MS)
-    return;
-
-  int16_t strength = soundStrength();
-  ledShow(strength);
-
-  if (strength > MAX_SOUND_STRENGTH && now >= lastnoise + MAX_SOUND_INTERVAL_MS) {
-    State = (State == STOP) ? GO : STOP;
-    lastnoise = now;
-  }
-
-  lastcall = now;
-}
-
-void onWall() {
-  static unsigned long lastcall = 0;
-  unsigned long now = millis();
-
-  if (State == STOP || now < lastcall + DEFAULT_INTERVAL_MS)
-    return;
-
-  double d = distanceCm();
-  if (d > 0.0 && d < MIN_DISTANCE_CM) {
-    if (State != TURN)
-      Direction = (0 == random(0, 2)) ? LEFT : RIGHT;
-    State = TURN;    
-  } else {  
-    State = GO;
-  }
-
-  lastcall = now;
-}
-
-void onState() {
-  switch (State) {
-    case STOP:
-      stopMotors();
-      break;
-
-    case TURN:
-      turn90();
-      break;
-
-    case GO:
-      moveForward();
-      break;
-  }
+    Tasks[i]();
+    i = (i + 1) % NTasks;
 }
 
 #endif
